8c5bdb5e322fe112fac3508312f519df51827212
laceejansson <laceejansson@gmail.com>
2020-10-25 19:30:05 -0600

    finished local storage for each row

--- script.js
+++ script.js
@@ -12,58 +12,60 @@ var now = currentTime()
 
 $(document).ready(function(){
     $(`#${hours}`).addClass("present");
+
     $("#9").val(localStorage.getItem("9"));
     if (hours >9) {
         $(`#9`).addClass("past");
     } 
+    $("#10").val(localStorage.getItem("10"));
     if (hours >10) {
         $(`#10`).addClass("past");
     } else if (hours < 10) {
         $(`#10`).addClass("future");
     }
-
+    $("#11").val(localStorage.getItem("11"));
     if (hours > 11) {
         $("#11").addClass("past");
     } else if (hours < 11) {
         $("#11").addClass("future");
     }
-
+    $("#12").val(localStorage.getItem("12"));
     if (hours > 12) {
         $("#12").addClass("past");
     } else if (hours < 12) {
         $("#12").addClass("future");
     }
-
+    $("#13").val(localStorage.getItem("13"));
     if (hours > 13) {
         $("#13").addClass("past");
     } else if (hours < 13) {
         $("#13").addClass("future");
     }
-
+    $("#14").val(localStorage.getItem("14"));
     if (hours > 14) {
         $("#14").addClass("past");
     } else if (hours < 14) {
         $("#14").addClass("future");
     }
-
+    $("#15").val(localStorage.getItem("15"));
     if (hours > 15) {
         $("#15").addClass("past");
     } else if (hours < 15) {
         $("#15").addClass("future");
     }
-
+    $("#16").val(localStorage.getItem("16"));
     if (hours > 16) {
         $("#16").addClass("past");
     } else if (hours < 16) {
         $("#16").addClass("future");
     }
-
+    $("#17").val(localStorage.getItem("17"));
     if (hours > 17) {
         $("#17").addClass("past");
     } else if (hours < 17) {
         $("#17").addClass("future");
     }
-
+    $("#18").val(localStorage.getItem("18"));
     if (hours > 18) {
         $("#18").addClass("past");
     } else if (hours < 18) {
