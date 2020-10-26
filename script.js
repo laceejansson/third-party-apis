ec086f579a92f4043dc18fd3020a0b43bba205be
laceejansson <laceejansson@gmail.com>
2020-10-25 19:02:04 -0600

    add background color to calendar input rows

--- script.js
+++ script.js
@@ -11,5 +11,64 @@ function currentTime() {
 var now = currentTime() 
 
 $(document).ready(function(){
-    $(`#${hours}`).css("background-color", "red")();
+    $(`#${hours}`).addClass("present");
+    if (hours >9) {
+        $(`#9`).addClass("past");
+    } 
+
+    if (hours >10) {
+        $(`#10`).addClass("past");
+    } else if (hours < 10) {
+        $(`#10`).addClass("future");
+    }
+
+    if (hours > 11) {
+        $("#11").addClass("past");
+    } else if (hours < 11) {
+        $("#11").addClass("future");
+    }
+
+    if (hours > 12) {
+        $("#12").addClass("past");
+    } else if (hours < 12) {
+        $("#12").addClass("future");
+    }
+
+    if (hours > 13) {
+        $("#13").addClass("past");
+    } else if (hours < 13) {
+        $("#13").addClass("future");
+    }
+
+    if (hours > 14) {
+        $("#14").addClass("past");
+    } else if (hours < 14) {
+        $("#14").addClass("future");
+    }
+
+    if (hours > 15) {
+        $("#15").addClass("past");
+    } else if (hours < 15) {
+        $("#15").addClass("future");
+    }
+
+    if (hours > 16) {
+        $("#16").addClass("past");
+    } else if (hours < 16) {
+        $("#16").addClass("future");
+    }
+
+    if (hours > 17) {
+        $("#17").addClass("past");
+    } else if (hours < 17) {
+        $("#17").addClass("future");
+    }
+
+    if (hours > 18) {
+        $("#18").addClass("past");
+    } else if (hours < 18) {
+        $("#18").addClass("future");
+    }
+    
   });
+
